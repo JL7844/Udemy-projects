2fd1df3dc70d23adc122806993a6bb3dbd603e88
Jl7844 <jossylegesse17@gmail.com>
2025-10-28 00:19:49 -0900

    updating the date-counter files

--- date-counter/src/counter.jsx
+++ date-counter/src/counter.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 function Counter(){
- const [step, setStep] = useState(1);
-  const [count, setCount]= useState(0);
+ const [step, setStep] = useState(0);
+  const [count, setCount]= useState("");
   const date = new Date('jun 21 2027');
   date.setDate(date.getDate() + count);
   
@@ -13,37 +13,32 @@ const handleDeclineCount = () => {
 
   setCount(c => c - step);
 };
-const handleInclineStep = () => {
-  setStep(s => s + 1);
 
-};
-const handleDeclineStep = () => {
-  setStep(s => s - 1);
-};
+const handleChange = (e) => {
+  setStep(Number(e.target.value));
+}
+const handleInput = (e) => {
+  setCount(Number(e.target.value));
+}
   return(
     <>
     <div className='container'>
       <div>
-        <button onClick={handleDeclineStep}>
-               -
-        </button>
-            <p className='para'>
-              step: {step} </p>
-        <button onClick={handleInclineStep}>
-                +
-        </button>
+        <input value={step} type="range" min={0} max={10} onChange={handleChange}/>
+        <span>{step}</span>
       </div>
+      
         <div>
         <button onClick={handleDeclineCount}>
                 -
         </button>
-          <p className='para'>
-            count: {count} </p>
+          <input className='inputBox' type="number" placeholder='0' value={count} onChange={handleInput} />
         <button onClick={handleInclineCount}>
                 +
         </button>
         </div>
         <span>{count === 0 ? "today is" : count > 0 ? `${count} days from today is`: `${Math.abs(count)} days ago was`} {date.toDateString()} </span>
+        
     </div>
    
      
